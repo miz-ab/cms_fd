c0168792b416f33aef1bcdd1bd537149b1848314
miz-ab <mizaby19@gmail.com>
2024-11-24 22:34:08 +0300

    add btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
 import  {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 //import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -10,7 +11,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule, MatInputModule, MatFormFieldModule],
+  imports: [MatCardModule, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
