092fcb5c2e7725caceff0f4a27e6c528324a7bd8
miz-ab <mizaby19@gmail.com>
2024-11-24 12:23:04 +0300

    add toolbar, home, login, customer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { TestComponentComponent } from './coreComponents/test-component/test-component.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
-
+import  {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponentComponent, MatSlideToggleModule, MatButtonModule, MatDialogModule],
+  imports: [RouterOutlet, RouterLink, TestComponentComponent, MatSlideToggleModule, MatButtonModule,
+           MatDialogModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { LoginComponent } from './core/login/login.component';
+import { CustomerComponent } from './core/customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : 'home', component:HomeComponent},
+    {path: 'login', component:LoginComponent},
+    {path: 'customer', component:CustomerComponent}
+];
