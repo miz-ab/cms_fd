da1bed563f10d20895c872d4ae222d535655c6dc
miz-ab <mizaby19@gmail.com>
2024-11-25 16:41:32 +0300

    add login model

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -3,23 +3,27 @@ import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
 import { InputFieldComponent } from '../input-field/input-field.component';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModel } from '../../model/login-model.model';
 
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule, MatInputModule, MatFormFieldModule, MatButtonModule,InputFieldComponent, ReactiveFormsModule],
+  imports: [MatCardModule, MatInputModule, MatFormFieldModule, MatButtonModule,InputFieldComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   form: FormGroup;
+
+  _logindata: LoginModel = {
+    username: '',
+    password: ''
+  }
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -34,6 +38,21 @@ export class LoginComponent {
 
   get passwordControl(): FormControl {
     return this.form.get('password') as FormControl;
+  }
+
+  ProceedLogin(form: any) {
+    if (form.valid) {
+      // this.service.Proceedlogin(this._logindata).subscribe(item => {
+      //   let _resp = item;
+      //   if(_resp.length>0){
+      //     localStorage.setItem('username',this._logindata.username);
+      //     this.router.navigateByUrl('');
+      //   }else{
+      //     alert('Invalid credentials');
+      //   }
+      // })
+    }
+
   }
 
 }
